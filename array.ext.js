499dc519f3d9601239e4f1bf19c5c5c1a87b0a60
xizher <xizher@163.com>
2020-11-27 18:45:53 +0800

    feat: 扩展方式增加$前缀

--- array.ext.js
+++ array.ext.js
@@ -1,24 +1,26 @@
-Array.prototype.insert = function (index, item) {
+Array.prototype.$insert = function (index, item) {
   this.splice(index, 0, item)
 }
 
-Array.prototype.remove = function (index) {
+Array.prototype.$remove = function (index) {
+  const item = this[index]
   this.splice(index, 1)
+  return item
 }
 
-Array.prototype.clear = function () {
+Array.prototype.$clear = function () {
   this.splice(0, this.length)
 }
 
-Array.prototype.reset = function (...items) {
+Array.prototype.$reset = function (...items) {
   this.splice(0, this.length, ...items)
 }
 
-Array.prototype.removeByValue = function (val, { many = false } = {}) {
+Array.prototype.$removeByValue = function (val, { many = false } = {}) {
   const evalStr = `
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === val) {
-        this.splice(i, 1)
+      if (this[i] === ${val}) {
+        this.splice(i--, 1)
         ${many ? '' : 'break'}
       }
     }
@@ -26,11 +28,16 @@ Array.prototype.removeByValue = function (val, { many = false } = {}) {
   eval(evalStr)
 }
 
-Array.prototype.unique = function () {
+Array.prototype.$unique = function () {
+  this.splice(0, this.length)
+  this.push(...new Set(this))
+}
+
+Array.prototype.$getUnique = function () {
   return [...new Set(this)]
 }
 
-Array.prototype.equal = function (array) {
+Array.prototype.$equal = function (array) {
   if (this.length !== array.length) {
     return false
   }
@@ -42,7 +49,7 @@ Array.prototype.equal = function (array) {
   return true
 }
 
-Array.prototype.findItem = function (propName, propValue) {
+Array.prototype.$findItem = function (propName, propValue) {
   for (let i = 0; i < this.length; i++) {
     const item = this[i]
     if (item[propName] === propValue) {
@@ -52,7 +59,7 @@ Array.prototype.findItem = function (propName, propValue) {
   return null
 }
 
-Array.prototype.findItems = function (propName, propValue) {
+Array.prototype.$findItems = function (propName, propValue) {
   const result = []
   for (let i = 0; i < this.length; i++) {
     const item = this[i]
@@ -63,7 +70,7 @@ Array.prototype.findItems = function (propName, propValue) {
   return result
 }
 
-Array.prototype.propToArr = function (propName) {
+Array.prototype.$propToArr = function (propName) {
   const propArr = []
   for (let i = 0; i < this.length; i++) {
     propArr.push(this[i][propName])
@@ -71,24 +78,24 @@ Array.prototype.propToArr = function (propName) {
   return propArr
 }
 
-Array.prototype.sum = function () {
+Array.prototype.$sum = function () {
   let sum = 0
   this.forEach(item => sum += item)
   return sum
 }
 
-Array.prototype.last = function () {
+Array.prototype.$last = function () {
   return this[this.length - 1]
 }
 
-Array.prototype.max = function () {
+Array.prototype.$max = function () {
   return Math.max(...this)
 }
 
-Array.prototype.min = function () {
+Array.prototype.$min = function () {
   return Math.min(...this)
 }
 
-Array.prototype.ave = function () {
+Array.prototype.$ave = function () {
   return this.sum() / this.length
 }
